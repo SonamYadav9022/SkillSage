5091561dba685567a53b6326bf412ef187ec8e2e
SonamYadav9022 <sonamyadav246810@gmail.com>
2026-07-11 01:10:49 +0530

    Switch database to Postgres for production deployment

--- prisma/migrations/20260427160602_profile_fields_added/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "cgpa" TEXT;
-ALTER TABLE "users" ADD COLUMN "goal" TEXT;
-ALTER TABLE "users" ADD COLUMN "manualSkills" TEXT;
-ALTER TABLE "users" ADD COLUMN "tenthMarks" TEXT;
-ALTER TABLE "users" ADD COLUMN "twelfthMarks" TEXT;
